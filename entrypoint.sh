0c2fe4fa1feb8c32d6505d562c68032569e0f272
kinz <kinzunite@gmail.com>
2020-07-17 16:53:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,6 @@ echo "#### Work on Production #####"
 
 cd production
 python -m compileall . -q
-ls
 git add -A
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
 git push origin master --force
