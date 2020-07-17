ad825e4af1069d8c003eac0f3e4b2874c0fb96bf
kinz <kinzunite@gmail.com>
2020-07-17 16:01:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,23 @@ GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
 USER_EMAIL="$4"
 
-echo "Get Python Version"
+echo "#### Get Python Version ####"
 python --version
 
 echo "Check Directory"
 ls
 
-echo "Get Production Repo"
+echo "#### Get Production Repo ####"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" production
 
-echo "Cleaning Develop"
-cd production
+echo "#### Cleaning Develop ####"
+cd develop
 rm -rf .git
 cd ../
 
-echo "Merging Code"
+echo "#### Merging Code #####"
 cp -r -u develop production
 
 cd production
