ef98fa2936cf163da092c1dfc3c4d6d82d48b614
kinz <kinzunite@gmail.com>
2020-07-17 15:58:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,28 +6,26 @@ GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
 USER_EMAIL="$4"
 
-ls
-CLONE_DIR=$(mktemp -d)
+echo "Get Python Version"
+python --version
 
-# Setup git
+echo "Check Directory"
+ls
+
+echo "Get Production Repo"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" production
 
-echo "$CLONE_DIR"
+echo "Cleaning Develop"
+cd production
+rm -rf .git
+cd ../
 
-ls
-ls -la "$CLONE_DIR"
+echo "Merging Code"
+cp -r -u develop production
 
-# Copy files into the git and deletes all git
-find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
-
-ls -la "$CLONE_DIR"
-
-cp -r "$FOLDER"/* "$CLONE_DIR"
-
-cd "$CLONE_DIR"
-
+cd production
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
-git push origin master
+git push origin master force
